30c0ea376aaec63119ee9c8f9878e2a2bc8f4460
4nth0ny1 <acatullo4@gmail.com>
2023-12-25 15:52:51 -0600

    reformated files

--- main.py
+++ main.py
@@ -4,40 +4,45 @@ from settings import *
 from map import *
 from player import *
 
+
 class Game:
-        def __init__(self):
-            pg.init()
-            self.screen = pg.display.set_mode(RES)
-            self.clock = pg.time.Clock()
-            self.delta_time = 1
-            self.new_game()
+    def __init__(self):
+        pg.init()
+        self.screen = pg.display.set_mode(RES)
+        self.clock = pg.time.Clock()
+        self.delta_time = 1
+        self.new_game()
 
-        def new_game(self):
-            self.map = Map(self)
-            self.player = Player(self)
-        def update(self):
-            self.player.update()
-            pg.display.flip()
-            self.delta_time = self.clock.tick(FPS)
-            pg.display.set_caption(f'{self.clock.get_fps() :.1f}')
+    # test
 
-        def draw(self):
-            self.screen.fill('black')
-            self.map.draw()
-            self.player.draw()
+    def new_game(self):
+        self.map = Map(self)
+        self.player = Player(self)
 
-        def check_events(self):
-            if __name__ == '__main__':
-                for event in pg.event.get():
-                    if event.type == pg.QUIT or (event.type == pg.KEYDOWN and event.key == pg.K_ESCAPE):
-                        pg.quit()
-                        sys.exit()
+    def update(self):
+        self.player.update()
+        pg.display.flip()
+        self.delta_time = self.clock.tick(FPS)
+        pg.display.set_caption(f'{self.clock.get_fps() :.1f}')
 
-        def run(self):
-            while True:
-                self.check_events()
-                self.update()
-                self.draw()
+    def draw(self):
+        self.screen.fill('black')
+        self.map.draw()
+        self.player.draw()
+
+    def check_events(self):
+        if __name__ == '__main__':
+            for event in pg.event.get():
+                if event.type == pg.QUIT or (event.type == pg.KEYDOWN and event.key == pg.K_ESCAPE):
+                    pg.quit()
+                    sys.exit()
+
+    def run(self):
+        while True:
+            self.check_events()
+            self.update()
+            self.draw()
+
 
 if __name__ == '__main__':
     game = Game()
